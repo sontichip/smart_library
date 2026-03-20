0478761d4e7422f60149c83d6e65a64c4b61436d
sontichip <buingoctungson@gmail.com>
2026-03-20 14:43:47 +0700

    Fix: Add missing GoogleFonts import in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../models/book.dart';
 import '../services/api_service.dart';
 import 'book_detail_screen.dart';
